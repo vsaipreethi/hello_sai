47126c270e0a3532ea1cf5e4fe5a7a325c22da01
Hp <preethisaivungarala23@gmail.com>
2024-01-27 22:13:30 +0530

    The sum of num from $a1 to $a2 is

---
+++ src/main/scala/second_project.scala
@@ -0,0 +1,12 @@
+object second_project {
+  def main(args: Array[String]): Unit = {
+    val a1 = 56
+    val a2 = 153
+
+    val sum = calsum(a1, a2)
+
+    println(s"The sum of num from $a1 to $a2 is: $sum")}
+  def calsum(a1:Int, a2:Int):Int = {
+    (a1 to a2)sum
+  }
+}
